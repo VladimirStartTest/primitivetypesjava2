009f6631a8480e35c3a9ff48bf500276ce0eec0a
Vladimir QA <vladimirtyldikin@gmail.com>
2023-06-20 23:30:44 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int initialBalance = 100;
+        int addend = 1100;
+
+        int bonus = 0;
+        if (addend > 1000)
+            bonus = addend / 100;
+
+        int finalBalance = initialBalance + addend + bonus;
+
+        System.out.println("Итоговый баланс: " + finalBalance);
+        System.out.println("Бонус: " + bonus);
+
+    }
+}
